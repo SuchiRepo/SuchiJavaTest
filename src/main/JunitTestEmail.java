07d04cd8d7642dca2fcc96a7ed1a7bf359d20dea
“suchirepo” <suchismitalenkaa@gmail.com>
2017-03-29 10:45:29 -0500

    testEmail

---
+++ src/main/JunitTestEmail.java
@@ -0,0 +1,16 @@
+
+
+
+public class JunitTest() {
+
+    private int var1=1;
+
+    @Test
+    public add(int var1) {
+
+        var1=1+1;
+        System.out.println("var1"+var1);
+        System.out.println("Hello test");
+        }
+
+        }
